ea76ad4d66f0d2c5ee14b20fcda0550427852fff
v1hate <herrero.julian.galeano@gmail.com>
2024-10-21 19:59:22 -0300

    urls

--- init.py
+++ init.py
@@ -10,6 +10,6 @@ db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 
 # Configuración de CORS para permitir el acceso desde el front-end
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Cambia esto por tu URL de producción
+CORS(app, resources={r"/*": {"origins": "https://pr2vic.netlify.app"}})  # Cambia esto por tu URL de producción
 
 from app import routes, models
